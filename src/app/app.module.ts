b967d8a90bcbc0fb6201aa830839c2c8506b3fb0
Fernando Parmezani dos Reis <tetris.freis@terceiros.mag.com.br>
2022-02-24 13:33:18 -0300

    include dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app.routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { ToastrModule } from 'ngx-toastr';
+import { DatePipe } from '@angular/common';
 
 import { GerarCpfComponent } from './components/gerar-cpf/gerar-cpf.component';
 import { GerarCnpjComponent } from './components/gerar-cnpj/gerar-cnpj.component';
@@ -16,6 +17,7 @@ import { VejatambemComponent } from './shared/vejatambem/vejatambem.component';
 import { GerarCnhComponent } from './components/gerar-cnh/gerar-cnh.component';
 import { GerarPisPasepComponent } from './components/gerar-pispasep/gerar-pispasep.component';
 import { ContatoComponent } from './shared/contato/contato.component';
+import { DashboardComponent } from './shared/dashboard/dashboard.component';
 
 @NgModule({
     declarations: [
@@ -28,9 +30,10 @@ import { ContatoComponent } from './shared/contato/contato.component';
         GerarPisPasepComponent,
         VejatambemComponent,
         ContatoComponent,
+        DashboardComponent,
     ],
     imports: [ BrowserModule, AppRoutingModule, FormsModule, ToastrModule.forRoot(), HttpClientModule ],
-    providers: [],
+    providers: [ DatePipe ],
     bootstrap: [ AppComponent ],
 })
 export class AppModule {}
